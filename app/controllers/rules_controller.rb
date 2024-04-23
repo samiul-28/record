785965d3b2cbe1585fa6b74db75707c52494ba09
samiul-alim <s.a.samiru64@gmail.com>
2024-04-23 11:44:24 +0600

    2nd commmit

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,20 +1,20 @@
 class RulesController < ApplicationController
   def index
-    @rules
+    @rules = Rule.all
   end
 
-  def show
-  end
+  # def show
+  # end
 
-  def new
-  end
+  # def new
+  # end
 
-  def create
-  end
+  # def create
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 end
